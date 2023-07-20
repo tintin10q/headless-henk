70e0dd4a4b96707bb59101ae4297fe857635a814
Quinten Cabo <tintin10q@hotmail.com>
2023-07-20 21:36:07 +0200

    better config loading, only loading once and showing the uri and also warning if it is not the default one. Use load_config to get it

--- config.py
+++ config.py
@@ -2,8 +2,16 @@ import toml
 from dataclasses import dataclass
 from typing import TypedDict
 
-with open("config.toml", "r") as config_file:
-    config_dict = toml.load(config_file)
+from colors import RED, RESET, BLUE, PURPLE, YELLOW
+
+import os
+
+if not os.path.exists('./config.toml'):
+    print(PURPLE, 'config.toml', RESET + YELLOW + "file not found. Created empty config file please fill it in auth_token", RESET)
+    with open('config.toml', 'w+') as config_file:
+        starter_config = {"auth_token": 'ENTER TOKEN HERE!', 'chief_host': 'chief.placenl.nl'}
+        toml.dump(starter_config, config_file)
+    exit(0)
 
 
 class Brand(TypedDict):
@@ -12,12 +20,18 @@ class Brand(TypedDict):
     name: str
 
 
+default_reddit_uri = 'https://gql-realtime-2.reddit.com/query'
+default_chief_host = 'chief.placenl.nl'
+
+
 @dataclass
 class Config:
-    chief_host: str
-    author: str = "quinten_cabo"
+    auth_token: str
+    chief_host: str = default_chief_host
+    author: str = "quinten_c"
     version: str = '0.1.0'
     name: str = 'placeNL_headless'
+    reddit_uri: str = default_reddit_uri
 
     def get_brand_payload(self) -> Brand:
         brand = {
@@ -29,13 +43,36 @@ class Config:
         return brand
 
 
+__config = None
+
+
 def load_config() -> Config:
+    global __config
+
+    if __config is not None:
+        return __config
+
+    with open("config.toml", "r") as config_file:
+        config_dict = toml.load(config_file)
+
     match config_dict:
-        case {"chief_host": str(chief_host)}:
-            return Config(chief_host)
+        # Deze heeft chief host, reddit_host en auth_token
+        case {"chief_host": str(chief_host), "reddit_host": str(reddit_host), "auth_token": str(auth_token)}:
+            print(BLUE, "Starting with chief host:", RESET, PURPLE, chief_host, RESET, YELLOW + "Custom chief host be careful" + RESET if chief_host != default_chief_host else "")
+            print(BLUE, "Starting with reddit api host:", RESET, PURPLE, reddit_host, RESET, YELLOW + "Custom reddit host be careful" + RESET if reddit_host != default_reddit_uri else "")
+            if not auth_token.startswith("Bearer "):
+                print(RED, "Invalid auth token, it doesn't start with 'Bearer '", RESET)
+                exit(1)
+            __config = Config(auth_token=auth_token, chief_host=chief_host)
+        # Deze heeft alleen chief host
+        case {"chief_host": str(chief_host), "auth_token": str(auth_token)}:
+            print(BLUE, "Starting with chief host:", RESET, PURPLE, chief_host, RESET, YELLOW + "Custom chief host be careful" + RESET if chief_host != default_chief_host else "")
+            print(BLUE, "Starting with reddit api host:", RESET, PURPLE, default_reddit_uri, RESET)
+            if not auth_token.startswith("Bearer "):
+                print(RED, "Invalid auth token, it doesn't start with 'Bearer '", RESET)
+                exit(1)
+            __config = Config(auth_token=auth_token, chief_host=chief_host)
+            return Config(auth_token=auth_token, chief_host=chief_host)
         case _:
-            raise ValueError(f"invalid configuration: {config_dict}, we need client_host fields")
-
-
-# clean up locals
-del config_file
+            print(f"{RED}Invalid configuration: we need {RESET}{BLUE}chief_host{RESET} {RED}fields{RESET}")
+            exit(1)
