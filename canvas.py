970c44e5ec10f8a7b5351a4f7ad1de99e5f64263
Quinten Cabo <tintin10q@hotmail.com>
2023-07-23 23:54:14 +0200

    Made the i an input to the get_canvaspart coroutine instead of taking the global value which will always be 5

--- canvas.py
+++ canvas.py
@@ -41,10 +41,11 @@ async def build_canvas_image(image_ids: List[Literal[0, 1, 2, 3, 4, 5, None]], u
     get_canvas_coroutines = []
     for i, image_id in enumerate(image_ids):
         if image_id is not None:
-            async def get_canvas_i():
+            async def get_canvaspart(index):
                 canvas_part = await get_canvas_part(image_id, username=username)
-                canvas_parts[i] = canvas_part
-            get_canvas_coroutines.append(get_canvas_i())
+                canvas_parts[index] = canvas_part
+
+            get_canvas_coroutines.append(get_canvaspart(i))
 
     await asyncio.gather(*get_canvas_coroutines)
 
